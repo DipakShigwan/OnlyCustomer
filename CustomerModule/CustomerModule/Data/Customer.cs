5f37d74dfe304c52626c20b593d52fd6f9b7a44c
Dipak Shigwan <shigwandipak123@gmail.com>
2022-11-02 19:27:56 +0530

    initial

---
+++ CustomerModule/CustomerModule/Data/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerModule.Data
+{
+    [Table("Customer")]
+    public partial class Customer
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+        public int? ContactNumber { get; set; }
+        [Column(TypeName = "date")]
+        public DateTime? RegisteredDate { get; set; }
+    }
+}
---
+++ CustomerModule/CustomerModule/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CustomerModule.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
